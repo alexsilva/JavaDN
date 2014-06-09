7594c3f5698149fe7da01ad71059c9a67872bc22
alex <geniofuturo@gmail.com>
2014-06-09 00:01:10 -0300

    Code reformat.

--- lib_class/src/com/example/JavaDN/Math.java
+++ lib_class/src/com/example/JavaDN/Math.java
@@ -1,19 +1,18 @@
 package com.example.JavaDN;
 
-import com.example.JavaDN.IMath;
-
 /**
  * Sum: number object.
  */
 public class Math implements IMath {
 
-    public Math() {}
+    public Math() {
+    }
 
     public static void main(String[] args) {
         System.out.println("com.example.JavaDN.Math");
     }
 
-    public static boolean gte(int a, int b){
+    public static boolean gte(int a, int b) {
         return a > b;
     }
 
--- src/com/example/JavaDN/HttpRequest.java
+++ src/com/example/JavaDN/HttpRequest.java
@@ -1,13 +1,14 @@
 package com.example.JavaDN;
 
-import android.util.Log;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.*;
-import java.net.URL;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * HttpRequest: Dumps to OutputStream
